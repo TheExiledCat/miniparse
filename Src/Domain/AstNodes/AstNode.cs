bf84cc8472fcce54b5f651bbaf5057ace0c47d63
TheExiledCat <kazekage2002@hotmail.com>
2026-02-21 00:31:58 +0100

    accept

--- Src/Domain/AstNodes/AstNode.cs
+++ Src/Domain/AstNodes/AstNode.cs
@@ -5,5 +5,8 @@ namespace Domain.AstNodes;
 // The bass class for every abstract syntax tree (AST) node
 public abstract class AstNode
 {
-    public abstract void Accept(IAstVisitor visitor);
+    public void Accept(IAstVisitor visitor)
+    {
+        visitor.Visit(this);
+    }
 }
