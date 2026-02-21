cb4bd8b0fcdb353e346a185b1edbd73dd4627224
TheExiledCat <kazekage2002@hotmail.com>
2026-02-21 02:43:02 +0100

    fix

--- Src/Domain/AstNodes/AstNode.cs
+++ Src/Domain/AstNodes/AstNode.cs
@@ -5,5 +5,5 @@ namespace Domain.AstNodes;
 // The bass class for every abstract syntax tree (AST) node
 public abstract class AstNode
 {
-    public abstract void Accept(IAstVisitor visitor);
+    public abstract T Accept<T>(IAstVisitor<T> visitor);
 }
--- Src/Visitors/IAstVisitor.cs
+++ Src/Visitors/IAstVisitor.cs
@@ -1,8 +1,9 @@
+using Domain.AstNodes;
+
 namespace Visitors;
 
-public interface IAstVisitor
+public interface IAstVisitor<T>
 {
-
     //add a single visit function for each node type you want to be able to visit
     // each visit optionally returns a node which can be used for editing the tree
     // for most cases this will just be 'return node'
@@ -12,4 +13,12 @@ public interface IAstVisitor
     AstNode Visit(BinaryNode node);
     etc...
     */
+
+    public T Visit(AddNode node);
+    public T Visit(AssignmentNode node);
+    public T Visit(ConstantNode node);
+    public T Visit(DivideNode node);
+    public T Visit(MultiplyNode node);
+    public T Visit(SubtractNode node);
+    public T Visit(VariableNode node);
 }
